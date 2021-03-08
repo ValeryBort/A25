6545137fda98bca6f47fe58c70509bc5d10d40c6
Valery Bortnikova <valery.ivlieva@gmail.com>
2021-03-08 12:41:43 +0300

    Job application test modified

--- test_job_application.py
+++ test_job_application.py
@@ -1,21 +1,30 @@
-import selenium
-import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 from selenium.webdriver import Chrome
 
 
-def test_chek_placeholder():
+def test_job_application_is_available():
+    """
+    Анкета соискателя на должность тестировщика доступна
+    """
     with Chrome() as driver:
         driver.get("https://a25.ru/about/vakansii/")
         driver.maximize_window()
-        time.sleep(3)
-        button1 = driver.find_element_by_xpath('//a[text()="Тестировщик"]')
+        button1 = WebDriverWait(driver, 5).until(
+            EC.element_to_be_clickable((By.XPATH, '//a[text()="Тестировщик"]'))
+        )
         button1.click()
-        time.sleep(1)
         driver.execute_script("window.scrollTo(0, window.scrollY + 1200)")
-        time.sleep(3)
-        button2 = driver.find_element_by_xpath('//a[contains(text(), "Хочу быть тестировщиком в А25")]')
+        button2 = WebDriverWait(driver, 5).until(
+            EC.element_to_be_clickable((By.XPATH, '//a[contains(text(), "Хочу быть тестировщиком в А25")]'))
+        )
         button2.click()
-        time.sleep(3)
-        driver.execute_script("window.scrollTo(0, window.scrollY + 500)")
-        time.sleep(3)
+        new_window = driver.window_handles[1]
+        driver.switch_to.window(new_window)
+        header = WebDriverWait(driver, 5).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "h1"))
+        )
+
+        assert header.text == "Работа в команде А25", "Job application page is not available"
